31db93e1def871035a6b155c00a8bc8083ba3b5a
gleb <novodran@gmail.com>
2018-03-07 23:18:39 +0200

    GWTransform : work in progress

--- src/GWTransform.cpp
+++ src/GWTransform.cpp
@@ -6,4 +6,36 @@
 #include "GWVector.hpp"
 #include "GWMatrix.hpp"
 #include "GWTransform.hpp"
+#include "GWQuaternion.hpp"
 
+template<typename T> void GWTransform<T>::make_transform(const GWQuaternionBase<T>& rot, const GWVectorBase<T>& trn, const GWVectorBase<T>& scl, GWTransformOrder order) {
+	const uint8_t SCL = 0;
+	const uint8_t ROT = 1;
+	const uint8_t TRN = 2;
+
+	static struct { uint8_t i0, i1, i2; } tbl[] = {
+		{ SCL, ROT, TRN },
+		{ SCL, TRN, ROT },
+		{ ROT, SCL, TRN },
+		{ ROT, TRN, SCL },
+		{ TRN, SCL, ROT },
+		{ TRN, ROT, SCL }
+	};
+
+	GWTransform m[3];
+	m[SCL].make_scaling(scl);
+	m[TRN] = rot.get_transform();
+	m[ROT].make_translation(trn);
+
+	uint32_t ord = (uint32_t)order;
+	int i0 = tbl[ord].i0;
+	int i1 = tbl[ord].i1;
+	int i2 = tbl[ord].i2;
+
+	*this = m[i0];
+	mul(m[i1]);
+	mul(m[i2]);
+}
+
+template void GWTransform<float>::make_transform(const GWQuaternionBase<float>& rot, const GWVectorBase<float>& trn, const GWVectorBase<float>& scl, GWTransformOrder order);
+template void GWTransform<double>::make_transform(const GWQuaternionBase<double>& rot, const GWVectorBase<double>& trn, const GWVectorBase<double>& scl, GWTransformOrder order);
--- src/GWTransform.hpp
+++ src/GWTransform.hpp
@@ -2,9 +2,22 @@
  * Author: Gleb Novodran <novodran@gmail.com>
  */
 
+template<typename T> class GWQuaternionBase;
+
 template<typename T> class GWTransform {
 public:
 	T m[4][4];
+
+	void set_zero() {
+		T* pData = &m[0][0];
+		for (int i = 0; i < 16; ++i) {
+			pData[i] = 0.0f;
+		}
+	}
+
+	void set_identity() {
+		make_scaling(1, 1, 1);
+	}
 
 	void set_row(uint32_t idx, const GWVectorBase<T>& v, T pad = 0) {
 		for (int i = 0; i < 3; ++i) {
@@ -13,12 +26,59 @@ public:
 		m[idx][3] = pad;
 	}
 
+	void set_translation(const GWVectorBase<T>& v) {
+		set_row(3, v, 1);
+	}
+
+	void set_translation(T tx, T ty, T tz) {
+		m[3][0] = tx;
+		m[3][1] = ty;
+		m[3][2] = tz;
+		m[3][3] = T(1);
+	}
+
+	void make_translation(T tx, T ty, T tz) {
+		set_zero();
+		set_translation(tx, ty, tz);
+	}
+
+	void make_translation(const GWVectorBase<T>& trn) {
+		T* pData = &m[0][0];
+		make_translation(trn.x, trn.y, trn.z);
+	}
+
+	void make_scaling(T sx, T sy, T sz) {
+		T* pData = &m[0][0];
+		set_zero();
+		m[0][0] = sx;
+		m[1][1] = sy;
+		m[2][2] = sz;
+		m[3][3] = T(1);
+	}
+
+	void make_scaling(const GWVectorBase<T>& scl) {
+		T* pData = &m[0][0];
+		make_scaling(scl.x, scl.y, scl.z);
+	}
+
+	void make_transform(const GWQuaternionBase<T>& rot, const GWVectorBase<T>& trn, const GWVectorBase<T>& scl, GWTransformOrder order = GWTransformOrder::SRT);
+
+	void mul(const GWTransform& m0, const GWTransform& m1) {
+		GWTransform res;
+		GWMatrix::mul_mm(&res.m[0][0], &m0.m[0][0], &m1.m[0][0], 4, 4, 4);
+		(*this) = res;
+	}
+	void mul(const GWTransform& m) { mul(*this, m); }
+
+	void invert(const GWTransform& m);
+	void invert();
+
+	void transpose(const GWTransform& m);
+	void transpose();
+
 	GWVectorBase<T> calc_vec(const GWVectorBase<T>& v) const {
 		GWTuple4<T> res;
-		GWTuple4<T> vec;
-		GWTuple::copy(vec, v);
-		vec[3] = 0;
-		GWMatrix::mul_vm(res.elems, vec.elems, &m[0][0], 4, 4);
+		GWMatrix::mul_vm(res.elems, v.elems, &m[0][0], 3, 4);
 		return GWVectorBase<T>(res);
 	}
 
@@ -31,6 +91,12 @@ public:
 		return GWVectorBase<T>(res);
 	}
 };
+
+template<typename T> inline GWTransform<T> operator * (const GWTransform<T>& m0, const GWTransform<T>& m1) {
+	GWTransform<T> m;
+	m.mul(m0, m1);
+	return m;
+}
 
 typedef GWTransform<float> GWTransformF;
 typedef GWTransform<double> GWTransformD;
