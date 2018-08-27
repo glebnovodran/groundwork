81cbec635a1368d94832a3a57067bdd645ef6fc6
gleb <novodran@gmail.com>
2018-08-27 23:41:45 +0300

    GWList changes

--- src/GWList.hpp
+++ src/GWList.hpp
@@ -8,46 +8,64 @@ template<typename T> struct GWListItem {
 	char* mpName;
 	T* mpPrev;
 	T* mpNext;
+
 	GWListItem() : mNameHash(), mpName(nullptr), mpPrev(nullptr), mpNext(nullptr) {}
+	void set_name(const char* pName) {
+		mpName = pName;
+		mNameHash.calculate(pName);
+	}
 };
 
 template<typename ITEM_T> class GWNamedObjList {
+protected:
+	ITEM_T* mpHead;
+	ITEM_T* mpTail;
+	uint32_t mCount;
+
 public:
 	GWNamedObjList() {}
 	~GWNamedObjList() {}
 
-	void add(ITEM_T* pItem) {
-		insert_before(mpTop);
+	void link(ITEM_T* pItem) {
+		if (pItem == nullptr) { return; }
+
+		pItem->mpPrev = nullptr;
+		pItem->mpNext = nullptr;
+
+		if (mpHead == nullptr) {
+			mpHead = pItem;
+		} else {
+			pItem->mpPrev = mpTail;
+		}
+		if (mpTail != nullptr) {
+			mpTail->mpNext = pItem;
+		}
+		mpTail = pItem;
+		++mCount;
 	}
-	void remove(ITEM_T* pItem) {
-		if (pItem->mpPrev) { pItem->mpPrev->mpNext = pItem->mpNext; }
-		if (pItem->mpNext) { pItem->mpNext->mpPrev = pItem->mpPrev; }
+
+	void unlink(ITEM_T* pItem) {
+		if (pItem == nullptr) { return; }
+
+		ITEM_T pNext = pItem->mpNext;
+		if (pItem->mpPrev == nullptr) {
+			mpHead = pNext;
+			if (pNext == nullptr) {
+				mpTail = nullptr;
+			} else {
+				pNext->mpPrev = nullptr;
+			}
+		} else {
+			pItem->mpPrev->mpNext = pNext;
+			if (pNext == nullptr) {
+				mpTail = pItem->mpPrev;
+			} else {
+				pNext->mpPrev = pItem->mpPrev;
+			}
+		}
+		--mCount;
 	}
 
 	ITEM_T* find(const char* pName);
 	ITEM_T* find_next(const ITEM_T* pItem);
-protected:
-
-	void insert_after(ITEM_T* pWhere, ITEM_T* pItem) {
-		pItem->mpPrev = pWhere;
-		pItem->mpNext = pWhere->mpNext;
-		pWhere->mpNext = pItem;
-		if (pItem->mpNext != nullptr) {
-			pItem->mpNext->mpPrev = pItem;
-		}
-	}
-
-	void insert_before(ITEM_T* pWhere, ITEM_T* pItem) {
-		pItem->mpPrev = pWhere->mpPrev;
-		pItem->mpNext = pWhere;
-		pWhere->mpPrev = pItem;
-		if (pItem->mpPrev != nullptr) {
-			pItem->mpPrev->mpNext = pItem;
-		}
-	}
-
-protected:
-	ITEM_T* mpTop;
-private:
-
 };
