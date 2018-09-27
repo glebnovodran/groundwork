6dcd6d2825ab35372aea913f63064fd8174f7f8e
Gleb Novodran <novodran@gmail.com>
2018-09-27 19:33:45 +0300

    GWMatrix : custom precision for 'inner' operations; lu_improve

--- src/GWMatrix.hpp
+++ src/GWMatrix.hpp
@@ -1,6 +1,7 @@
 /*
  * Author: Gleb Novodran <novodran@gmail.com>
  */
+#include <cmath>
 
 namespace GWMatrix {
 	// Based on https://blogs.msdn.microsoft.com/nativeconcurrency/2014/09/04/raking-through-the-parallelism-tool-shed-the-curious-case-of-matrix-matrix-multiplication/
@@ -53,9 +54,23 @@ namespace GWMatrix {
 		}
 	}
 
+	// https://en.wikipedia.org/wiki/Pascal_matrix
+	template<typename T>
+	inline void gen_Pascal_mtx(T* pMtx, int n) {
+		using namespace GWBase;
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				int64_t x = factorial(i + j) / (factorial(i) * factorial(j));
+				pMtx[i*n + j] = T(x);
+			}
+		}
+	}
+
 	/* transpose */
 
-	template<typename DST_T, typename SRC_T> void transpose(DST_T* pDst, SRC_T* pSrc, int N) {
+	template<typename DST_T, typename SRC_T>
+	inline void transpose(DST_T* pDst, SRC_T* pSrc, int N) {
 		for (int i = 0; i < N - 1; ++i) {
 			for (int j = i + 1; j < N; ++j) {
 				int ij = i * N + j;
@@ -67,7 +82,7 @@ namespace GWMatrix {
 		}
 	}
 
-	template<typename DST_T> void transpose(DST_T* pMtx, int N) {
+	template<typename DST_T> inline void transpose(DST_T* pMtx, int N) {
 		transpose(pMtx, pMtx, N);
 	}
 
@@ -174,77 +189,87 @@ namespace GWMatrix {
 
 	/* inner products */
 
-	template<typename T>
+	template<typename T, typename CALC_T = T>
 	inline T inner_row_vec(const T* pMtx, const int ncol, const int irow, const T* pVec, const int iorg, const int iend) {
-		T s = T(0);
+		CALC_T s = CALC_T(0);
 		const T* pRow = pMtx + (irow * ncol);
 		for (int i = iorg; i <= iend; ++i) {
-			s += pRow[i] * pVec[i];
+			CALC_T a = pRow[i];
+			CALC_T b = pVec[i];
+			s += a * b;
 		}
-		return s;
+		return T(s);
 	}
 
-	template<typename T>
+	template<typename T, typename CALC_T = T>
 	inline T inner_row_vec(const T* pMtx, const int ncol, const int irow, const T* pVec) {
-		return inner_row_vec(pMtx, ncol, irow, pVec, 0, ncol - 1);
+		return inner_row_vec<T, CALC_T>(pMtx, ncol, irow, pVec, 0, ncol - 1);
 	}
 
-	template<typename T>
+	template<typename T, typename CALC_T = T>
 	inline T inner_col_vec(const T* pMtx, const int ncol, const int icol, const T* pVec, const int iorg, const int iend) {
-		T s = T(0);
+		CALC_T s = CALC_T(0);
 		for (int i = iorg; i <= iend; ++i) {
 			const T* pCol = pMtx + (i * ncol) + icol;
-			s += *pCol * pVec[i];
+			CALC_T a = *pCol;
+			CALC_T b = pVec[i];
+			s += a * b;
 		}
-		return s;
+		return T(s);
 	}
 
-	template<typename T>
+	template<typename T, typename CALC_T = T>
 	inline T inner_col_vec(const T* pMtx, const int ncol, const int icol, const T* pVec) {
-		return inner_col_vec(pMtx, ncol, icol, pVec, 0, ncol - 1);
+		return inner_col_vec<T, CALC_T>(pMtx, ncol, icol, pVec, 0, ncol - 1);
 	}
 
-	template<typename T>
+	template<typename T, typename CALC_T = T>
 	inline T inner_row_row(const T* pMtxA, const int ncolA, const int irowA, const T* pMtxB, const int ncolB, const int irowB, int iorg, int iend) {
-		T s = T(0);
+		CALC_T s = CALC_T(0);
 		const T* pRowA = pMtxA + (irowA * ncolA);
 		const T* pRowB = pMtxB + (irowB * ncolB);
 		for (int i = iorg; i <= iend; ++i) {
-			s += pRowA[i] * pRowB[i];
+			CALC_T a = pRowA[i];
+			CALC_T b = pRowB[i];
+			s += a * b;
 		}
-		return s;
+		return T(s);
 	}
 
-	template<typename T>
+	template<typename T, typename CALC_T = T>
 	inline T inner_row_row(const T* pMtxA, const int ncol, const int irowA, const T* pMtxB, const int irowB) {
-		return inner_row_row(pMtxA, ncol, irowA, pMtxB, ncol, irowB, 0, ncol - 1);
+		return inner_row_row<T, CALC_T>(pMtxA, ncol, irowA, pMtxB, ncol, irowB, 0, ncol - 1);
 	}
 
-	template<typename T>
+	template<typename T, typename CALC_T = T>
 	inline T inner_row_row(const T* pMtx, const int ncol, const int irow1, const int irow2) {
-		return inner_row_row(pMtx, ncol, irow1, pMtx, irow2);
+		return inner_row_row<T, CALC_T>(pMtx, ncol, irow1, pMtx, irow2);
 	}
 
-	template<typename T>
+	template<typename T, typename CALC_T = T>
 	inline T inner_row_col(const T* pMtxA, const int ncolA, const int irowA, const T* pMtxB, const int ncolB, const int icolB, int iorg, int iend) {
-		T s = T(0);
+		CALC_T s = CALC_T(0);
 		const T* pRowA = pMtxA + (irowA * ncolA);
 		for (int i = iorg; i <= iend; ++i) {
 			const T* pColB = pMtxB + (i * ncolB) + icolB;
-			s += *pColB * pRowA[i];
+			CALC_T a = pRowA[i];
+			CALC_T b = *pColB;
+			s += a * b;
 		}
-		return s;
+		return T(s);
 	}
 
-	template<typename T>
+	template<typename T, typename CALC_T = T>
 	inline T inner_col_col(const T* pMtxA, const int ncolA, const int icolA, const T* pMtxB, const int ncolB, const int icolB, int iorg, int iend) {
-		T s = T(0);
+		CALC_T s = CALC_T(0);
 		for (int i = iorg; i <= iend; ++i) {
 			const T* pColA = pMtxA + (i * ncolA) + icolA;
 			const T* pColB = pMtxB + (i * ncolB) + icolB;
-			s += *pColA * *pColB;
+			CALC_T a = *pColA;
+			CALC_T b = *pColB;
+			s += a * b;
 		}
-		return s;
+		return T(s);
 	}
 
 
@@ -309,6 +334,43 @@ namespace GWMatrix {
 	}
 
 	template<typename T>
+	inline void lu_get_lower(T* pL, const T* pLU, const int n) {
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				int offs = i*n + j;
+				pL[offs] = j > i ? 0 : pLU[offs];
+			}
+		}
+	}
+
+	template<typename T>
+	inline void lu_get_upper(T* pU, const T* pLU, const int n) {
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				int offs = i*n + j;
+				if (i == j) {
+					pU[offs] = 1;
+				} else if (j > i) {
+					pU[offs] = pLU[offs];
+				} else {
+					pU[offs] = 0;
+				}
+			}
+		}
+	}
+
+	template<typename T>
+	inline void lu_get_perm(T* pP, const int n, const int* pPerm) {
+		set_identity(pP, n);
+		for (int i = 0; i < n; ++i) {
+			int idx = pPerm[i];
+			if (idx != i) {
+				swap_rows(pP, n, i, idx);
+			}
+		}
+	}
+
+	template<typename T>
 	inline void lu_solve(T* pAns, const T* pLU, const int n, const int* pPerm, const T* pRH) {
 		if (pAns == nullptr) return;
 		if (pRH != nullptr) {
@@ -346,6 +408,20 @@ namespace GWMatrix {
 			det = -det;
 		}
 		return det;
+	}
+
+	template<typename T>
+	inline void lu_improve(/* inout */ T* pAns, const T* pMtx, const T* pLU, const int n, const int* pPerm, const T* pRH, T* pTmpVec) {
+		for (int i = 0; i < n; ++i) {
+			long double r = -pRH[i];
+			r += inner_row_vec<T, long double>(pMtx, n, i, pAns);
+			pTmpVec[i] = T(r);
+		}
+		lu_solve(pTmpVec, pLU, n, pPerm, pTmpVec);
+
+		for (int i = 0; i < n; ++i) {
+			pAns[i] -= pTmpVec[i];
+		}
 	}
 
 	template<typename T>
--- tests/calc_test/src/test_mtx.cpp
+++ tests/calc_test/src/test_mtx.cpp
@@ -7,6 +7,8 @@
 #include "test.hpp"
 
 static bool test_solve3() {
+	using namespace GWBase;
+
 	static float A[] = {
 		2, -1, 2,
 		1, -2, 1,
@@ -15,37 +17,75 @@ static bool test_solve3() {
 	static float b[] = {
 		10, 8, 11
 	};
+	/*
+		A = [2, -1, 2; 1, -2, 1; 3, -1, 2]
+		b = [10, 8, 11]'
+		A \ b
+	 */
 
 	float tmp[3];
 	int perm[3];
 	float LU[3 * 3];
-	GWMatrix::lu_decomp(LU, A, 3, tmp, perm);
+	float L[3 * 3];
+	float U[3 * 3];
+	float P[3 * 3];
+	float Q[3 * 3];
+	float PtL[3 * 3];
+	int ds = 0;
+
+	GWMatrix::lu_decomp(LU, A, 3, tmp, perm, &ds);
+	GWMatrix::lu_get_lower(L, LU, 3);
+	GWMatrix::lu_get_upper(U, LU, 3);
+	GWMatrix::lu_get_perm(P, 3, perm);
+	// A == P'*L*U
+	GWMatrix::transpose(P, 3);
+	GWMatrix::mul_mm(PtL, P, L, 3, 3, 3);
+	GWMatrix::mul_mm(Q, PtL, U, 3, 3, 3);
+	for (int i = 0; i < 3*3; ++i) {
+		int d = f32_ulp_diff(Q[i], A[i]);
+		if (d > 200) return false;
+	}
+
 	float ans[3];
 	GWMatrix::lu_solve(ans, LU, 3, perm, b);
+	GWMatrix::lu_improve(ans, A, LU, 3, perm, b, tmp);
+	float det = GWMatrix::lu_det(LU, 3, ds); // det(A)
+	if (f32_ulp_diff(det, 3.0f) > 100) {
+		return false;
+	}
 
 	static float expected[] = { 1, -2, 3 }; // A \ b
+	int dmax = 0;
 	for (int i = 0; i < 3; ++i) {
-		int d = GWBase::f32_ulp_diff(ans[i], expected[i]);
+		int d = f32_ulp_diff(ans[i], expected[i]);
+		dmax = std::max(d, dmax);
 		if (d > 200) return false;
 	}
 
 	float inv[3 * 3];
+	float v[3];
+#if 1
 	for (int i = 0; i < 3; ++i) {
-		float v[3];
 		for (int j = 0; j < 3; ++j) {
 			v[j] = float(i == j ? 1 : 0);
 		}
 		GWMatrix::lu_solve(&inv[i * 3], LU, 3, perm, v);
+		GWMatrix::lu_improve(&inv[i * 3], A, LU, 3, perm, v, tmp);
 	}
 	GWMatrix::transpose(inv, 3);
+#else
+	GWMatrix::lu_inv(inv, LU, 3, perm, v);
+#endif
 
 	float invExpected[] = { // inv(A)
 		-1, 0, 1,
 		0.33333f, -0.66667f, 0,
 		1.66667, -0.33333, -1
 	};
+	int dmaxInv = 0;
 	for (int i = 0; i < 3*3; ++i) {
-		int d = GWBase::f32_ulp_diff(inv[i], invExpected[i]);
+		int d = f32_ulp_diff(inv[i], invExpected[i]);
+		dmaxInv = std::max(d, dmaxInv);
 		if (d > 200) return false;
 	}
 
