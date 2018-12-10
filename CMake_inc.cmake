82efa19fcea517b7b3242d190a00e26859799785
Gleb Novodran <novodran@gmail.com>
2018-12-10 14:35:16 +0200

    CMake build cleanup

--- CMake_inc.cmake
+++ CMake_inc.cmake
@@ -1,8 +1,7 @@
-set(GCC_RELEASE_COMPILE_FLAGS "-std=c++11 -O3 -mavx2 -mfpmath=sse -ffast-math -ftree-vectorize -mfma")
-#set(GCC_RELEASE_LINK_FLAGS    "")
+set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
+set(GCC_RELEASE_COMPILE_FLAGS "-O3 -mavx2 -mfpmath=sse -ffast-math -ftree-vectorize -mfma")
 
-set(GCC_DEBUG_COMPILE_FLAGS "-std=c++11 -ggdb -mavx2 -mfpmath=sse -ffast-math -ftree-vectorize -mfma")
-#set(GCC_RELEASE_LINK_FLAGS    "")
+set(GCC_DEBUG_COMPILE_FLAGS " -ggdb -mavx2 -mfpmath=sse -ffast-math -ftree-vectorize -mfma")
 
 set (CMAKE_CXX_FLAGS_RELEASE ${GCC_RELEASE_COMPILE_FLAGS} )
 set(CMAKE_EXE_LINKER_FLAGS_RELEASE  "${CMAKE_EXE_LINKER_FLAGS} ${GCC_RELEASE_COMPILE_FLAGS}" )
@@ -17,6 +16,5 @@ if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   set(CMAKE_BUILD_TYPE "${default_build_type}" cache
       string "Choose the type of build." FORCE)
   set_property(cache CMAKE_BUILD_TYPE PROPERTY STRINGS
-	"Debug" "Release")
-	#"Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+	"Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 endif()
