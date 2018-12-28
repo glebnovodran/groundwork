60d3aad88abc16ad3049685641228a611eb3d6e2
Gleb Novodran <novodran@gmail.com>
2018-12-28 15:53:06 +0200

    small fix

--- CMake_inc.cmake
+++ CMake_inc.cmake
@@ -13,8 +13,8 @@ set(default_build_type "Release")
 
 if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
-  set(CMAKE_BUILD_TYPE "${default_build_type}" cache
+  set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE
       string "Choose the type of build." FORCE)
-  set_property(cache CMAKE_BUILD_TYPE PROPERTY STRINGS
+  set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
 	"Debug" "Release" "MinSizeRel" "RelWithDebInfo")
 endif()
