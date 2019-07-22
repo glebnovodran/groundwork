ead9a79a39b67726ed2927fd3121e6317377b0b6
Gleb Novodran <novodran@gmail.com>
2019-07-22 17:59:41 +0300

    tdmot.py fix

--- py/tdmot.py
+++ py/tdmot.py
@@ -2,13 +2,23 @@ import os
 import sys
 import hou
 
+def getTrkData(trk, firstFrame, lastFrame, data):
+	for ifrm in range(firstFrame, lastFrame+1): data.append(trk.evalAtFrame(ifrm))
+
 def saveTDMot(path, chop, rows = True):
 	f = open(path, "w")
 	if not f: return
+
+	smpRange = chop.sampleRange()
+	firstFrame = int(chop.samplesToFrame(smpRange[0]))
+	lastFrame =int(chop.samplesToFrame(smpRange[1]))
+
 	if rows:
 		for i, trk in enumerate(chop.tracks()):
 			f.write(trk.name())
-			for val in trk.allSamples():
+			data = []
+			getTrkData(trk, firstFrame, lastFrame, data);
+			for val in data:
 				f.write("\t{0:.08f}".format(val))
 			f.write('\n')
 	else:
@@ -18,8 +28,10 @@ def saveTDMot(path, chop, rows = True):
 		f.write('\n')
 		smps = []
 		nfrm = 0
+
 		for trk in chop.tracks():
-			data = trk.allSamples()
+			data = []
+			getTrkData(trk, firstFrame, lastFrame, data);
 			nfrm = max(nfrm, len(data))
 			smps.append(data)
 		ntrk = len(chop.tracks())
