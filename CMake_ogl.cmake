380cf6a57943f6e78e2d96f13cc91f4d51b9539b
Gleb Novodran <novodran@gmail.com>
2019-10-31 14:49:27 +0200

    OGL shader make/validation skeleton

--- CMake_ogl.cmake
+++ CMake_ogl.cmake
@@ -8,8 +8,8 @@ target_include_directories (${PROJECT_NAME} PUBLIC "${POWERVR_SDK_ROOT}/external
 
 add_custom_command(OUTPUT ${GPU_DST_DIR}/gpu.done
 	COMMAND export GLSL=${GLSL_VALIDATOR} && export GPU_DST_DIR=${GPU_DST_DIR}
-	&& ${CMAKE_CURRENT_SOURCE_DIR}/src/gpu/a.sh
-	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/src/gpu"
+	&& ${CMAKE_CURRENT_SOURCE_DIR}/src/gpu/ogl/mk.sh
+	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/src/gpu/ogl"
 )
 
 add_custom_target(${PROJECT_NAME}_gpu
---
+++ samples/gl_demo/src/gpu/ogl/mk.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+function shader() {
+	local outname=$1
+	echo $outname
+	shift
+	echo "==========="
+	while test ${#} -gt 0
+	do
+		echo "	${1}"
+		cat ${1} >> $outname
+		shift
+	done
+	${GLSL} $outname
+}
+
+function frag_shader() {
+	echo "frag shader"
+	echo $@:0
+	shader $1 prologue_frag.h "${@:2}"
+}
+
+function vtx_shader() {
+	echo "vtx shader"
+	shader $1 prologue_vert.h "${@:2}"
+}
+
+echo "Shader validation at ..."
+pwd
+#echo "${GLSL}"
+if [ -z "$GPU_DST_DIR" ]; then
+	echo "GPU_DST_DIR is not set"
+	export GPU_DST_DIR=.
+fi
+
+if [ -z "${GLSL}" ]; then
+	echo "Path to GLSL validator is not set"
+	export GLSL=/projects/VulkanSDK/1.1.114.0/x86_64/bin/glslangValidator
+fi
+#else
+	echo "Using $GLSL"
+	vtx_shader ${GPU_DST_DIR}/skin.vert skin.vert
+	frag_shader ${GPU_DST_DIR}/mtl.frag simple.frag
+	touch ${GPU_DST_DIR}/gpu.done
+#fi
+
