8e9332cc032353628daf92535f0f81a9abc8c657
Gleb Novodran <novodran@gmail.com>
2019-11-13 15:07:54 +0200

    remove shader validation task from the main build

--- CMake_ogl.cmake
+++ CMake_ogl.cmake
@@ -5,15 +5,3 @@ set(GPU_DST_DIR ${CMAKE_BINARY_DIR}/bin/data/gpu)
 
 target_include_directories (${PROJECT_NAME} PUBLIC "${POWERVR_SDK_ROOT}/include")
 target_include_directories (${PROJECT_NAME} PUBLIC "${POWERVR_SDK_ROOT}/external/glm")
-
-add_custom_command(OUTPUT ${GPU_DST_DIR}/gpu.done
-	COMMAND export GLSL=${GLSL_VALIDATOR} && export GPU_DST_DIR=${GPU_DST_DIR}
-	&& ${CMAKE_CURRENT_SOURCE_DIR}/src/gpu/ogl/mk.sh
-	WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/src/gpu/ogl"
-)
-
-add_custom_target(${PROJECT_NAME}_gpu
-	DEPENDS ${GPU_DST_DIR}/gpu.done
-)
-
-add_dependencies(${PROJECT_NAME} ${PROJECT_NAME}_gpu)
