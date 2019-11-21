d5387d21260fd8e81a8b02b216006e8ce39e32c0
Gleb Novodran <novodran@gmail.com>
2019-11-21 20:43:24 +0200

    CMake build cleanup

--- CMake_ogl.cmake
+++ CMake_ogl.cmake
@@ -1,7 +1,6 @@
-set(POWERVR_SDK_ROOT "/opt/Imagination/PowerVR_Graphics/PowerVR_SDK/SDK_2019_R1.1")
-set(GLSL_VALIDATOR "/projects/VulkanSDK/1.1.114.0/x86_64/bin/glslangValidator")
+#set(POWERVR_SDK_ROOT "/opt/Imagination/PowerVR_Graphics/PowerVR_SDK/SDK_2019_R1.1")
+#set(GLSL_VALIDATOR "/projects/VulkanSDK/1.1.114.0/x86_64/bin/glslangValidator")
 
-set(GPU_DST_DIR ${CMAKE_BINARY_DIR}/bin/data/gpu)
+set(GPU_DST_DIR ${CMAKE_BINARY_DIR}/bin/data/gpu/ogl)
 
-target_include_directories (${PROJECT_NAME} PUBLIC "${POWERVR_SDK_ROOT}/include")
-target_include_directories (${PROJECT_NAME} PUBLIC "${POWERVR_SDK_ROOT}/external/glm")
+#target_include_directories (${PROJECT_NAME} PUBLIC "${POWERVR_SDK_ROOT}/include")
