fea2c74109dfbb8f3262e17e33d12cf44827b887
Gleb Novodran <novodran@gmail.com>
2020-12-03 16:50:10 +0200

    GWIntersect.hpp cleanup

--- src/GWIntersect.hpp
+++ src/GWIntersect.hpp
@@ -3,7 +3,9 @@
  */
 
 namespace GWIntersect {
-	template<typename T> bool seg_tri_ccw_alt(const GWVectorBase<T>& p, const GWVectorBase<T>& q,
+
+	// Ericson, Real-Time Collision Detection 2nd ed., p. 191
+	template<typename T> bool seg_tri_ccw(const GWVectorBase<T>& p, const GWVectorBase<T>& q,
 		const GWVectorBase<T>& a, const GWVectorBase<T>& b, const GWVectorBase<T>& c, GWVectorBase<T>* pHitPos, GWVectorBase<T>* pHitNrm) {
 
 		GWVectorBase<T> ab = b - a;
@@ -28,47 +30,6 @@ namespace GWIntersect {
 		}
 		if (pHitNrm) {
 			n.normalize();
-			*pHitNrm = n;
-		}
-		return true;
-	}
-
-	template<typename T> bool seg_tri_ccw(const GWVectorBase<T>& p, const GWVectorBase<T>& q,
-		const GWVectorBase<T>& v0, const GWVectorBase<T>& v1, const GWVectorBase<T>& v2, GWVectorBase<T>* pHitPos, GWVectorBase<T>* pHitNrm) {
-
-		GWVectorBase<T> edge[3];
-		GWVectorBase<T> vp[3];
-		GWVectorBase<T> dir = q - p;
-		edge[0] = v1 - v0;
-		vp[0] = p - v0;
-		GWVectorBase<T> n = GWVector::cross(edge[0], v2 - v0);
-		n.normalize();
-		T d0 = vp[0].dot(n);
-		T d1 = (q - v0).dot(n);
-
-		if (d0*d1 > T(0.0f) || (d0 == 0.0f && d1 == T(0.0f))) return false;
-
-		edge[1] = v2 - v1;
-		edge[2] = v0 - v2;
-		vp[1] = p - v1;
-		vp[2] = p - v2;
-
-		if (GWVector::triple(edge[0], dir, vp[0]) < T(0.0f)) { return false; }
-		if (GWVector::triple(edge[1], dir, vp[1]) < T(0.0f)) { return false; }
-		if (GWVector::triple(edge[2], dir, vp[2]) < T(0.0f)) { return false; }
-
-		T d = dir.dot(n);
-		T t;
-		if ((d == T(0.0f)) || (d0 == T(0.0f))) {
-			t = T(0.0f);
-		} else {
-			t = -d0/d;
-		}
-		if (t > T(1.0f) || t < T(0.0f)) return false;
-		if (pHitPos) {
-			*pHitPos = p + t * dir;
-		}
-		if (pHitNrm) {
 			*pHitNrm = n;
 		}
 		return true;
